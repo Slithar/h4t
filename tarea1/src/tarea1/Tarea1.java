6436fbac836117b91eeb592b4c4a23c4118a3dfb
Mauro <Mauro@Mauro-PC>
2016-08-04 23:12:35 -0300

    6 y media am. he terminado. te odio ariel!

--- tarea1/src/tarea1/Tarea1.java
+++ tarea1/src/tarea1/Tarea1.java
@@ -17,6 +17,7 @@ public class Tarea1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola git");
+        System.out.println("I have succesfully pushed this");
     }
     
 }
